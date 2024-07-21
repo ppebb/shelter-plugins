c2e500af4d9d67dfdfc672317e9abc96163d718d
ppeb <ppeb@ppeb.me>
2024-07-21 03:43:29 -0500

    eslintrc fix

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+export default [
+    {
+        rules: {
+            indent: ["error", 4],
+            "no-unused-vars": "warn",
+            semi: ["warn", "always"],
+            quotes: ["warn", "double"],
+        }
+    }
+];
