7f70289b2a4e6129f3a8a28f5beae14fe7980843
ppebb <ppeb@ppeb.me>
2025-03-26 11:36:36 -0500

    (reparent-user-panel) Continue trying to reparent until success

--- plugins/reparent-user-panel/index.tsx
+++ plugins/reparent-user-panel/index.tsx
@@ -1,25 +1,40 @@
-export function onLoad() {
+function tryReparent(): boolean {
     const userArea = document.querySelector("[aria-label='User area']");
     const sidebar = document.querySelector(".sidebarList_c48ade");
     const guilds = document.querySelector("[aria-label='Servers sidebar']") as HTMLElement;
 
     if (!userArea) {
         console.error("Unable to query user area with [aria-label='User area']");
-        return;
+        return false;
     }
 
     if (!sidebar) {
         console.error("Unable to query sidebar with .sidebarList_c48ade");
-        return;
+        return false;
     }
 
     if (!guilds) {
         console.error("Unable to query guilds with [aria-label='Servers sidebar']");
-        return;
+        return false;
     }
 
     userArea.remove();
     sidebar.appendChild(userArea);
     guilds.style.marginBottom = "0px";
     guilds.style.paddingBottom = "8px";
+
+    return true;
+}
+
+function runTimeout() {
+    if (tryReparent())
+        return;
+
+    setTimeout(() => {
+        runTimeout();
+    }, 500);
+}
+
+export function onLoad() {
+    runTimeout();
 }
