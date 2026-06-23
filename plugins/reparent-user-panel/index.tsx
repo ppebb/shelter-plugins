50e73ff7ae5d50f6e0e7c74deb1a0b5d0185c849
ppeb <ppeb@ppeb.me>
2026-06-23 12:54:21 -0500

    Fix reparent-user-panel

--- plugins/reparent-user-panel/index.tsx
+++ plugins/reparent-user-panel/index.tsx
@@ -1,15 +1,15 @@
 function tryReparent(): boolean {
-    const userArea = document.querySelector("[aria-label='User area']");
-    const sidebar = document.querySelector(".sidebarList_c48ade");
+    const userArea = document.querySelector("[aria-label='User status and settings']");
+    const sidebar = document.querySelector(".sidebarList__5e434");
     const guilds = document.querySelector("[aria-label='Servers sidebar']") as HTMLElement;
 
     if (!userArea) {
-        console.error("Unable to query user area with [aria-label='User area']");
+        console.error("Unable to query user area with [aria-label='User status and settings']");
         return false;
     }
 
     if (!sidebar) {
-        console.error("Unable to query sidebar with .sidebarList_c48ade");
+        console.error("Unable to query sidebar with .sidebarList__5e434");
         return false;
     }
 
